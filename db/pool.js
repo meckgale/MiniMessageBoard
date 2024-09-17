efd884143ea850a57c0857242355c7c92f044d63
meck <meckgalen@gmail.com>
2024-09-17 22:40:06 +0300

    port value change

--- db/pool.js
+++ db/pool.js
@@ -3,10 +3,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
   database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432,
+  port: 5432,
   ssl: {
     rejectUnauthorized: false, // Required for hosted PostgreSQL (Koyeb)
   },
--- db/populatedb.js
+++ db/populatedb.js
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS messages (
 async function main() {
   const client = new Client({
     host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
     database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT || 5432,
+    port: 5432,
   });
 
   await client.connect();
