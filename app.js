d35f6ad95e02aee54f7363363ab0c83db7870af5
meck <meckgalen@gmail.com>
2024-09-17 16:55:28 +0300

    port changed to 8000

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
 app.use("/", messageRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
 });
